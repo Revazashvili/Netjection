e0e38327e1bd2ddef2eadc42432e373e73cc35f1
Revazashvili <levanrevazashvili@gmail.com>
2022-03-25 15:12:48 +0400

    feat(Netjection.UnitTests): add ServiceCollectionExtensionsTest.cs

---
+++ tests/Netjection.UnitTests/Extensions/ServiceCollectionExtensionsTest.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Netjection.Extensions;
+using Netjection.UnitTests.SampleServices;
+using Xunit;
+
+namespace Netjection.UnitTests.Extensions;
+
+public class ServiceCollectionExtensionsTest
+{
+    [Fact]
+    public void Should_Inject_All_Service()
+    {
+        var services = new ServiceCollection();
+        services.InjectServices(Assembly.GetExecutingAssembly());
+    }
+
+    [Fact]
+    public void Should_Inject_And_Resolve_Services()
+    {
+        var services = new ServiceCollection();
+        services.InjectServices(Assembly.GetExecutingAssembly());
+
+        var serviceProvider = services.BuildServiceProvider();
+        var firstService = serviceProvider.GetService<IFirstService>();
+        Assert.NotNull(firstService);
+    }
+    
+    [Fact]
+    public void Should_Call_Resolved_Service_Method()
+    {
+        var services = new ServiceCollection();
+        services.InjectServices(Assembly.GetExecutingAssembly());
+
+        var serviceProvider = services.BuildServiceProvider();
+        var firstService = serviceProvider.GetService<IFirstService>();
+        const string expected = "Test";
+        var actual = firstService?.Test();
+        Assert.Equal(expected,actual);
+    }
+}

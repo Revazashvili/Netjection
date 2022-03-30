365f9fea01763f7fb9204f0bf8f2123f4c87acca
Revazashvili <levanrevazashvili@gmail.com>
2022-03-30 08:46:26 +0400

    make InjectableBaseAttribute.cs target Class too

--- src/Netjection/Attributes/InjectableBaseAttribute.cs
+++ src/Netjection/Attributes/InjectableBaseAttribute.cs
@@ -4,7 +4,7 @@ namespace Netjection;
 /// Base attribute class
 /// </summary>
 /// <remarks>do not use if you want to make class injectable. use <see cref="InjectableAttribute"/> or scope based attributes.</remarks>
-[AttributeUsage(AttributeTargets.Interface)]
+[AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class)]
 public class InjectableBaseAttribute : Attribute
 {
     public InjectableBaseAttribute(Type? implementationType)

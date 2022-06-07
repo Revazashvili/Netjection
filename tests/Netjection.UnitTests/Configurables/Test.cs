37b01ffb0f94836b776733473819a5f609a980b8
revazashvili <levanrevazashvili@gmail.com>
2022-06-07 09:07:06 +0400

    add tests for several cases

---
+++ tests/Netjection.UnitTests/Configurables/Test.cs
@@ -0,0 +1,9 @@
+namespace Netjection.UnitTests.Configurables;
+
+[Configure]
+public class Test
+{
+    public string Property1 { get; set; }
+    public int Property2 { get; set; }
+    public string Property3 { get; set; }
+}

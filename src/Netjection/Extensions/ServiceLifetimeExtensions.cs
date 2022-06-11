4e1a99c141a3044038ae2b0a939ea8370f182b84
revazashvili <levanrevazashvili@gmail.com>
2022-06-11 17:05:04 +0400

    add xml comments for ServiceLifetimeExtensions

--- src/Netjection/Extensions/ServiceLifetimeExtensions.cs
+++ src/Netjection/Extensions/ServiceLifetimeExtensions.cs
@@ -2,8 +2,17 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Netjection;
 
+/// <summary>
+/// Extension class for <see cref="Lifetime"/>.
+/// </summary>
 internal static class ServiceLifetimeExtensions
 {
+    /// <summary>
+    /// Maps <see cref="Lifetime"/> to <see cref="ServiceLifetime"/>
+    /// </summary>
+    /// <param name="lifetime">The <see cref="Lifetime"/>.</param>
+    /// <returns><see cref="ServiceLifetime"/> that matches current instance of <see cref="Lifetime"/>.</returns>
+    /// <exception cref="Exception"><see cref="Lifetime"/> does not match any <see cref="ServiceLifetime"/></exception>
     internal static ServiceLifetime MapToServiceLifetime(this Lifetime lifetime) =>
         lifetime switch
         {
@@ -13,6 +22,13 @@ internal static class ServiceLifetimeExtensions
             _ => throw new Exception("can't match any service lifetime")
         };
     
+    /// <summary>
+    /// Maps <see cref="Lifetime"/> to <see cref="ServiceLifetime"/>
+    /// </summary>
+    /// <param name="attribute">The attribute type of <see cref="InjectableBaseAttribute"/>.</param>
+    /// <typeparam name="T">type of <see cref="InjectableBaseAttribute"/></typeparam>
+    /// <returns><see cref="ServiceLifetime"/> that matches current instance of <see cref="Lifetime"/>.</returns>
+    /// <exception cref="Exception"><see cref="Lifetime"/> does not match any <see cref="ServiceLifetime"/></exception>
     internal static ServiceLifetime MapToServiceLifetime<T>(this T attribute) where T : InjectableBaseAttribute =>
         attribute switch
         {

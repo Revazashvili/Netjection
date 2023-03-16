89daabebb1bb28f8b51bfb05e7342bcc6cf31746
revazashvili <levanrevazashvili@gmail.com>
2023-03-16 20:39:30 +0400

    update xml comments

--- src/Netjection/Services/IInjectableTypesProvider.cs
+++ src/Netjection/Services/IInjectableTypesProvider.cs
@@ -5,10 +5,10 @@ namespace Netjection;
 internal interface IInjectableTypesProvider
 {
     /// <summary>
-    /// Provides types based on passed attribute type from assembly.
+    /// Provides types decorated with a specific attribute from an assembly.
     /// </summary>
-    /// <param name="assembly">Assembly to search for injectable services.</param>
-    /// <param name="attributeType">The attribute type of <see cref="InjectableBaseAttribute"/>.</param>
-    /// <returns>Attribute decorated types from assembly.</returns>
+    /// <param name="assembly">The assembly to search for injectable services.</param>
+    /// <param name="attributeType">The attribute type to search for.</param>
+    /// <returns>A collection of types decorated with the specified attribute.</returns>
     IEnumerable<Type> GetTypesWithAttribute(Assembly assembly,Type attributeType);
 }
